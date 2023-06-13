f9814569e1a72f49e8319d3b9eec9ec57a93b364
wilbispaulo <wilbispaulo@hotmail.com>
2023-06-13 00:29:26 -0300

    primeiro commit

---
+++ src/BRCode.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace wilbispaulo\BRcode;
+
+require_once "helpers.php";
+
+define("CHAVE", "2601");
+define("NOME", "59");
+define("CIDADE", "60");
+define("TXID", "6205");
+define("CRC16", "63");
+define("POLINOMIO", 0x1021);
+define("INICIAL", 0xffff);
+define("TIPO", "ascii");
+
+/**
+ * Cria uma string 'PIX copia e cola' no padrão BR CODE v2.0.1
+ */
+
+class BRcode
+{
+    
+    protected $arranjo = array();
+
+    public function __construct()
+    {
+        $this->arranjo = array(
+            ['00', '01'],                   //Payload Format Indicator
+            ['2600', 'BR.GOV.BCB.PIX'],     //Merchant Account Information-GUI
+            ['2601', '1234567'],            //Merchant Account Information-Chave
+            ['52', '0000'],                 //Merchant Category Code
+            ['53', '986'],                  //Transaction Currency (R$)
+            ['58', 'BR'],                   //Country Code
+            ['59', 'FULANO DE TAL'],        //Merchant Name
+            ['60', 'BRASILIA'],             //Merchant City
+            ['6205', '***'],                //Additional Data Field Template-TXid
+            ['63', '1D3D']                  //CRC16 Pol 0x1021 CI 0xFFFF ordem modifica CRC
+        );
+    }
+
+    public function setChave(string $chave)
+    {
+        $id = array_search(CHAVE, array_column($this->arranjo, 0));
+        $this->arranjo[$id][1] = $chave;
+    }
+
+    public function setName(string $nome)
+    {
+        $id = array_search(NOME, array_column($this->arranjo, 0));
+        $nome = strtoupper(limpa($nome));
+        $this->arranjo[$id][1] = $nome;
+    }
+
+    public function setCidade(string $cidade)
+    {
+        $id = array_search(CIDADE, array_column($this->arranjo, 0));
+        $cidade = strtoupper(limpa($cidade));
+        $this->arranjo[$id][1] = $cidade;
+    }
+
+    public function setTxId(string $txid)
+    {
+        $id = array_search(TXID, array_column($this->arranjo, 0));
+        $this->arranjo[$id][1] = $txid;
+    }
+
+    public function gerar(): string
+    {
+        $brcode = "";
+        $subBrCode = "";
+        $idAnt = "";
+        $tamSubBrCode = "";
+        $colId = array_column($this->arranjo, 0);
+
+        foreach($colId as $key => $nomeId)
+        {
+            $tam = strlen($this->arranjo[$key][1]);
+            $tam = sprintf("%02d", $tam);
+            $id = substr($nomeId, 0, 2);
+            if ($id != $idAnt) {
+                $brcode .= ($idAnt . $tamSubBrCode . $subBrCode);
+                $subBrCode = "";
+                $idAnt = "";
+                $tamSubBrCode = "";
+            }
+            if ($id == "63") {
+                $brcode .= ($id . "04");
+                $crc16 = crc16($brcode, POLINOMIO, INICIAL, TIPO);
+                $brcode .= $crc16;
+            } else {
+                if (strlen($nomeId) == 2) {
+                    $brcode .= ($id . $tam . $this->arranjo[$key][1]);
+                } else {
+                    $idd = substr($nomeId, 2, 2);
+                    $subBrCode .= ($idd . $tam . $this->arranjo[$key][1]);
+                    $tamSubBrCode = sprintf("%02d", strlen($subBrCode));
+                    $idAnt = $id;
+                }
+            }
+        }
+        return $brcode;
+    }
+}
---
+++ src/helpers.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * Formata uma sring como url amigável
+ * @param string $string
+ * @return string
+ */
+function limpa(string $string):string
+{
+    $table = array(
+        'Š'=>'S', 'š'=>'s', 'Đ'=>'Dj', 'đ'=>'dj', 'Ž'=>'Z', 'ž'=>'z', 'Č'=>'C', 'č'=>'c', 'Ć'=>'C', 'ć'=>'c',
+        'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+        'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O',
+        'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss',
+        'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e',
+        'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o',
+        'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b',
+        'ÿ'=>'y', 'Ŕ'=>'R', 'ŕ'=>'r', '\\'=>'',
+    );
+    $slug_table = strtr($string, $table);
+    $slug_trim = trim($slug_table);
+    $slug_tag = strip_tags($slug_trim);
+    $slug_space = preg_replace('/[@%\[\]{}()\'\"`´^~|<>,.:;?+-\/]/', '', $slug_tag);
+    $slug = strtolower($slug_space);
+    return $slug;
+}
+
+function crc16($str, $polynomial, $initValue, $strType="hex", $xOrValue = 0, $inputReverse = false, $outputReverse = false) {
+    $crc = $initValue;
+    if($strType == "hex"){
+        $str = pack('H*', $str);
+    }
+    for ($i = 0; $i < strlen($str); $i++) {
+        if ($inputReverse) {
+            // Cada byte de dados de entrada é revertido bit a bit
+            $c = reverseChar($str[$i]);
+            $c = ord((string)$c);
+        } else {
+            $c = ord($str[$i]);
+        }
+        $crc ^= ($c << 8);
+        for ($j = 0; $j < 8; ++$j) {
+            if ($crc & 0x8000) {
+                $crc = (($crc << 1) & 0xffff) ^ $polynomial;
+            } else {
+                $crc = ($crc << 1) & 0xffff;
+            }
+        }
+    }
+    if ($outputReverse) {
+        // Armazene o endereço baixo no bit baixo, ou seja, use o método little endian para converter o inteiro em uma string
+        $ret = pack('cc', $crc & 0xff, ($crc >> 8) & 0xff);
+        // O resultado de saída é uma string inteira invertida em bits
+        $ret = reverseString($ret);
+        // Em seguida, converta o resultado de volta para um número inteiro de acordo com o método little endian
+        $arr = unpack('vshort', $ret);
+        $crc = $arr['short'];
+    }
+    return strtoupper(base_convert($crc ^ $xOrValue, 10, 16));
+}
+
+/**
+ * bit-reverse um fluxo de bytes eg: 'AB'(01000001 01000010)  --> '\x42\x82'(01000010 10000010)
+ * @param $str
+ */
+function reverseString($str) {
+    $m = 0;
+    $n = strlen($str) - 1;
+    while ($m <= $n) {
+        if ($m == $n) {
+            $str[$m] = reverseChar($str[$m]);
+            break;
+        }
+        $ord1 = reverseChar($str[$m]);
+        $ord2 = reverseChar($str[$n]);
+        $str[$m] = $ord2;
+        $str[$n] = $ord1;
+        $m++;
+        $n--;
+    }
+    return $str;
+}
+
+/**
+ * Inverte um caractere pouco a pouco eg: 65 (01000001) --> 130(10000010)
+ * @param $char
+ * @return $char
+ */
+function reverseChar($char) : string {
+    $byte = ord($char);
+    $tmp = 0;
+    for ($i = 0; $i < 8; ++$i) {
+        if ($byte & (1 << $i)) {
+            $tmp |= (1 << (7 - $i));
+        }
+    }
+    return chr($tmp);
+}
