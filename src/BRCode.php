ce53b9fa5c97a552cb8bb12fdaeaa04ae9c698f6
wilbispaulo <wilbispaulo@hotmail.com>
2024-02-05 23:12:03 -0300

    alteração geral

--- src/BRCode.php
+++ src/BRCode.php
@@ -2,113 +2,178 @@
 
 namespace wilbispaulo\BRcode;
 
-require_once "helpers.php";
-
-define("CHAVE", "2601");
-define("VALOR", "54");
-define("NOME", "59");
-define("CIDADE", "60");
-define("TXID", "6205");
-define("CRC16", "63");
-define("POLINOMIO", 0x1021);
-define("INICIAL", 0xffff);
-define("TIPO", "ascii");
-
-/**
- * Cria uma string 'PIX copia e cola' no padrão BR CODE v2.0.1
- */
-
 class BRcode
 {
-    
-    protected $arranjo = array();
+    protected $data = [
+        '00' => '01',
+        '26' => ['00' => 'BR.GOV.BCB.PIX'],
+        '52' => '0000',
+        '53' => '986',
+        '58' => 'BR',
+        '63' => ''
+    ];
 
-    public function __construct()
+    public function __construct(string $chave, string $nome, string $cidade, string $txId = "***", string $valor = "")
     {
-        $this->arranjo = array(
-            ['00', '01'],                   //Payload Format Indicator
-            ['2600', 'BR.GOV.BCB.PIX'],     //Merchant Account Information-GUI
-            ['2601', '1234567'],            //Merchant Account Information-Chave
-            ['52', '0000'],                 //Merchant Category Code
-            ['53', '986'],                  //Transaction Currency (R$)
-            ['54', ''],                     //Amount (R$)
-            ['58', 'BR'],                   //Country Code
-            ['59', 'FULANO DE TAL'],        //Merchant Name
-            ['60', 'BRASILIA'],             //Merchant City
-            ['6205', '***'],                //Additional Data Field Template-TXid
-            ['63', '1D3D']                  //CRC16 Pol 0x1021 CI 0xFFFF ordem modifica CRC
-        );
+        $chave = $this->validaChave($chave);
+        if ($chave === false) {
+            unset($this->data);
+            return $this->data = false;
+        }
+        $nome = $this->validaNome($nome);
+        var_dump($nome);
+        if ($nome === false) {
+            unset($this->data);
+            return $this->data = false;
+        }
+        $cidade = $this->validaCidade($cidade);
+        if ($cidade === false) {
+            unset($this->data);
+            return $this->data = false;
+        }
+        $txId = $this->validaTxId($txId);
+        if ($txId === false) {
+            unset($this->data);
+            return $this->data = false;
+        }
+        $valor = $this->validaValor($valor);
+        if ($valor === false) {
+            unset($this->data);
+            return $this->data = false;
+        }
+        $this->data["26"][CHAVE] = $chave;
+        $this->data[NOME] = $nome;
+        $this->data[CIDADE] = $cidade;
+        $this->data["62"][TXID] = $txId;
+        $this->data[VALOR] = $valor;
+    }
+
+    public function validaChave(string $chave): string | bool
+    {
+        if (strlen($chave) > MAXL_CHAVE) {
+            return false;
+        }
+        if (
+            !preg_match("/^([a-z0-9]+\.?)+[a-z0-9]@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/", $chave) &&
+            !preg_match("/^(\+55)[0-9]{11}$/", $chave) &&
+            !preg_match("/^([0-9a-f]{8})-([0-9a-f]{4}-){3}([0-9a-f]{12})$/", $chave)
+        ) {;
+            if (!$chave = (new Documento($chave))->isValid())
+                return false;
+        }
+        return $chave;
+    }
+
+    public function validaNome(string $nome)
+    {
+        $nome = strtoupper(limpa($nome));
+        $nome = substr($nome, 0, 25);
+
+        if (preg_match("/^[A-Z][A-Z ]+$/", $nome)) {
+            return $nome;
+        }
+        return false;
+    }
+
+    public function validaCidade(string $cidade)
+    {
+        $cidade = strtoupper(limpa($cidade));
+        $cidade = substr($cidade, 0, 15);
+
+        if (preg_match("/^[A-Z][A-Z ]+$/", $cidade)) {
+            return $cidade;
+        }
+        return false;
+    }
+
+    public function validaTxId(string $txId)
+    {
+        $txId = limpa($txId);
+        $txId = substr($txId, 0, 25);
+
+        if (preg_match("/^([a-zA-Z0-9]+)|([*]{3})$/", $txId)) {
+            return $txId;
+        }
+        return false;
+    }
+
+    public function validaValor(string $valor)
+    {
+        if ($valor == '') {
+            return $valor;
+        }
+        $valor = strip_tags(trim($valor));
+        if (preg_match("/^([0-9]{1,3})(\.[0-9]{3})*(,[0-9]{2})$/", $valor)) {
+            $tr = ["." => "", "," => "."];
+            $valor = strtr($valor, $tr);
+        }
+        if (preg_match("/^([0-9]{1,10})(\.[0-9]{2})$/", $valor)) {
+            return $valor;
+        }
+        return false;
     }
 
     public function setChave(string $chave)
     {
-        $id = array_search(CHAVE, array_column($this->arranjo, 0));
-        $this->arranjo[$id][1] = $chave;
+        $this->data[CHAVE] = $chave;
     }
 
     public function setNome(string $nome)
     {
-        $id = array_search(NOME, array_column($this->arranjo, 0));
         $nome = strtoupper(limpa($nome));
-        $this->arranjo[$id][1] = $nome;
+        $this->data[NOME] = $nome;
     }
 
     public function setCidade(string $cidade)
     {
-        $id = array_search(CIDADE, array_column($this->arranjo, 0));
         $cidade = strtoupper(limpa($cidade));
-        $this->arranjo[$id][1] = $cidade;
+        $this->data[CIDADE] = $cidade;
     }
 
     public function setTxId(string $txid)
     {
-        $id = array_search(TXID, array_column($this->arranjo, 0));
-        $this->arranjo[$id][1] = $txid;
+        $this->data[TXID] = $txid;
     }
 
     public function setValor(string $valor)
     {
-        $id = array_search(VALOR, array_column($this->arranjo, 0));
-        $this->arranjo[$id][1] = $valor;
+        $this->data[VALOR] = $valor;
     }
 
-    public function gerar(): string
+    public function geraPixCode(): string | bool
     {
-        $brcode = "";
-        $subBrCode = "";
-        $idAnt = "";
-        $tamSubBrCode = "";
-        $colId = array_column($this->arranjo, 0);
+        if (!$this->data) {
+            return false;
+        }
 
-        foreach($colId as $key => $nomeId)
-        {
-            $tam = strlen($this->arranjo[$key][1]);
-            if ($tam == 0) {
-                continue;
-            }
-            $tam = sprintf("%02d", $tam);
-            $id = substr($nomeId, 0, 2);
-            if ($id != $idAnt) {
-                $brcode .= ($idAnt . $tamSubBrCode . $subBrCode);
-                $subBrCode = "";
-                $idAnt = "";
-                $tamSubBrCode = "";
-            }
-            if ($id == "63") {
-                $brcode .= ($id . "04");
-                $crc16 = crc16($brcode, POLINOMIO, INICIAL, TIPO);
-                $brcode .= $crc16;
-            } else {
-                if (strlen($nomeId) == 2) {
-                    $brcode .= ($id . $tam . $this->arranjo[$key][1]);
+        $codePix = "";
+        $subCodePix = "";
+        $keys = ['00', '26', '52', '53', '54', '58', '59', '60', '62', '63'];
+        foreach ($keys as $key) {
+            if (!is_array($this->data[$key])) {
+                if ($key === '63') {
+                    $codePix .= '6304';
+                    $crc16 = CRC16($codePix, POLINOMIO, INICIAL, TIPO);
+                    $codePix .= $crc16;
+                } elseif ($this->data[$key] == '') {
+                    continue;
                 } else {
-                    $idd = substr($nomeId, 2, 2);
-                    $subBrCode .= ($idd . $tam . $this->arranjo[$key][1]);
-                    $tamSubBrCode = sprintf("%02d", strlen($subBrCode));
-                    $idAnt = $id;
+                    $codePix .= $key;
+                    $codePix .= sprintf("%02d", strlen($this->data[$key]));
+                    $codePix .= $this->data[$key];
                 }
             }
+            if (is_array($this->data[$key])) {
+                $id1 = array_keys($this->data[$key]);
+                foreach ($id1 as $id) {
+                    $subCodePix .= $id;
+                    $subCodePix .= sprintf("%02d", strlen($this->data[$key][$id]));
+                    $subCodePix .= $this->data[$key][$id];
+                }
+                $codePix .= $key . strlen($subCodePix) . $subCodePix;
+                $subCodePix = "";
+            }
         }
-        return $brcode;
+        return $codePix;
     }
 }
